33d7c1c8eca421209447d24d35db0b3d9093c283
Barkley <barkley.santo@gmail.com>
2022-08-18 14:25:22 +0800

    add global styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // where it goes
+  templateUrl: './app.component.html', // what it uses
+  styleUrls: ['./app.component.css']  // what it uses
 })
 export class AppComponent {
-  title = 'angular-tasks';
+  title:string = 'Task Tracker';
 }
