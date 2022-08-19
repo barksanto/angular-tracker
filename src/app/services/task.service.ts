b9da994625dcd6947ba2bb7d6ca66d0ae5586fb6
Barkley <barkley.santo@gmail.com>
2022-08-19 13:49:19 +0800

    successfully reading data from db.json instead of mock-data

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,17 +3,22 @@ import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 import {Observable, of} from 'rxjs'
 // service is just a class - Injectable declaration is being provided to the application level. 
+import {HttpClient, HttpHeaders} from "@angular/common/http" // angulars built in for http requests (like fetch)
 
 
-@Injectable({
+ @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5002/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // const tasks = of(TASKS);
+    // return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
+
+  
 }
