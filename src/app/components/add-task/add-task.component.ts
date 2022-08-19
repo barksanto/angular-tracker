d8e50376ed08da155f95c5267ef2d121415e132f
Barkley <barkley.santo@gmail.com>
2022-08-19 15:28:58 +0800

    reset form inputs on submit

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  // using !, try to initialize the array when you declare it next "makes: any[] = [];"
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+    if (!this.text) {
+      alert("Please add a task");
+      return
+    }
+    
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.text = "";
+    this.day = "";
+    this.reminder = false;
+
+    // @ TODO **emit event**
+  }
+
 
 }
