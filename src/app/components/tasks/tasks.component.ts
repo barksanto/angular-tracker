ff1a36bc03003fd6dd022baa83890d449f4bca89
Barkley <barkley.santo@gmail.com>
2022-08-19 13:31:42 +0800

    create localserver with json-server (added package) - server listens on port 5002 (was originally 5000, but disallowed because this port is used by another Mac service)

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../services/task.service';
+ // @ to use a service, I need to add it as a provider into the constructor
 import { Task } from '../../Task';
+
+
 
 @Component({
   selector: 'app-tasks',
@@ -10,11 +13,14 @@ import { Task } from '../../Task';
   
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS; // TASKS are a property of our component
+  tasks: Task[] = []; // TASKS are a property of our component
 
-  constructor() { }
+  constructor(private taskService:TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks
+    })
   }
 
 }
