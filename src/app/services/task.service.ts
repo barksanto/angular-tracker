ff7e3b1cd1011e98415a64959ca18d80141349de
Barkley <barkley.santo@gmail.com>
2022-08-19 15:02:37 +0800

    updateTask reminder implemented - ContentType = application/json - omitting json will break the code

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,11 @@ import {Observable} from 'rxjs'
 // service is just a class - Injectable declaration is being provided to the application level. 
 import {HttpClient, HttpHeaders} from "@angular/common/http" // angulars built in for http requests (like fetch)
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
  @Injectable({
   providedIn: 'root'
@@ -25,5 +30,9 @@ export class TaskService {
      return this.http.delete<Task>(url) // returns an observable
    }
 
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
   
 }
